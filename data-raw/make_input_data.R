4a43b5b00ae414291544324ac1cf281ad84893a3
Mark C. Wheldon <wheldon@un.org>
2025-04-29 18:04:08 -0400

    'make_input_data.R'
    - Created input survey data, country classifications and denominators.
    - NEXT: create input global estimates data.

--- data-raw/make_input_data.R
+++ data-raw/make_input_data.R
@@ -16,41 +16,82 @@
 ###-----------------------------------------------------------------------------
 ### * Setup
 
-library(FPEMglobal.aux)
-library(here)
+### Libraries
+
+## Check these are installed
+rlang::check_installed(c("FPEMglobal.aux", "fpemlocal", "here", "purrr",
+                         "usethis"),
+                       version = c("1.2.0", rep(NA, 4)))
+
+## Attached these packages
 library(magrittr)
 
-path_to_global_run <-
+### Paths
+
+path_to_global_run_mwra <-
     file.path(Sys.getenv("SharePoint_UN", unset = NA),
               "DESA-POP - PDU", "FPEM", "Results", "Released", "2024", "output",
               "240702_173547_15-49_married")
-stopifnot(dir.exists(path_to_global_run))
+stopifnot(dir.exists(path_to_global_run_mwra))
 
 ###-----------------------------------------------------------------------------
 ### * Survey Data
 
 ### Record the input file for reproducibility
-global_input_file_df <-
-    get_used_input_data(output_dir = path_to_global_run, variant = c("raw"))
-write.csv(global_input_file_df,
-          file = here("data-raw", "FPEMglobal_run_input_file.csv"),
-          row.names = FALSE)
+file.copy(from = file.path(path_to_global_run_mwra, "dataCPmodel_input_raw.csv"),
+          to = here::here("data-raw", "dataCPmodel_input_raw.csv"),
+          overwrite = TRUE)
 
 ### This part is a modified version of code from
 ### '[fpemlocal]/vignettes/developer_annual_data_updates.Rmd
 
 ## Read and impute
-contraceptive_use <- FPEMglobal.aux::input_data_2_fpemdata(output_dir = path_to_global_run) %>%
+contraceptive_use <-
+    FPEMglobal.aux::input_data_2_fpemdata(output_dir = path_to_global_run_mwra) %>%
     fpemlocal::impute_packagedata_se() %>%
         purrr::pluck("contraceptive_use_imputed")
 
-## Check the format
-fpemlocal_unpd_fork:::format_check(format_list = fpemlocal::contraceptive_use_format,
-                        data = contraceptive_use)
-
-## Save as package data if the format is correct
+## Save as package data
 usethis::use_data(contraceptive_use, overwrite = TRUE)
 
+###-----------------------------------------------------------------------------
+### * Denominators
 
+### Record the input file for reproducibility
+denom_fp <- FPEMglobal.aux::get_used_csv_denominators_filepath(path_to_global_run_mwra)
+file.copy(from = denom_fp,
+          to = here::here("data-raw", basename(denom_fp)),
+          overwrite = TRUE)
 
+population_counts <-
+    FPEMglobal.aux::denominators_2_fpemdata(output_dir = path_to_global_run_mwra)
+
+## Save as package data
+usethis::use_data(population_counts, overwrite = TRUE)
+
+###-----------------------------------------------------------------------------
+### * Country Classifications ("Divisions")
+
+### Record the input file for reproducibility
+cc_fname <- FPEMglobal::pkg_files_included(result = "filename")$model_run_inputs$region_information
+file.copy(from = file.path(path_to_global_run_mwra, cc_fname),
+          to = here::here("data-raw", cc_fname),
+          overwrite = TRUE)
+
+division_classifications <-
+    FPEMglobal.aux::country_classifications_2_fpemdata(path_to_global_run_mwra)
+
+## Save as package data
+usethis::use_data(division_classifications, overwrite = TRUE)
+
+###-----------------------------------------------------------------------------
+### * Global Run Results
+
+## These are only used in plots to compare one-country runs with global runs.
+
+!!!!!!!!!!!!!!!!!!!!!!!!! HERE HERE HERE
+
+global_estimates <-
+    FPEMglobal.aux::convert_csv_res_2_fpemdata(output_dir = path_to_global_run_mwra,
+                                               stat = "prop", adjusted = "sub_adj")
 
